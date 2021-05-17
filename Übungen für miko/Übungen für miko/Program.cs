46d2382f0c57822f2ed29865381d17465d832d0c
basti <84239503+basti-debug@users.noreply.github.com>
2021-05-17 08:50:44 +0200

    mikoübungen

---
+++ Übungen für miko/Übungen für miko/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Übungen_für_miko
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1.static public void Dreieck(int anzahl, string zeichen)
+             * Bsp. für einen Aufruf: Dreieck(4,"*")
+           
+             * ****
+             * ***
+             * **
+             * *
+             * 
+             * 2.static public double Maximum (double z1, double z2, double z3)
+             * größte der drei zahlen herausfinden
+             * 
+             * 3.static public double Summe(double sw, double mult, int anz)
+             * Bsp.: y = sume(1,2,64)       1+2+4+8+....
+             *       y = summe(2,3,5)       2+6+48+54+162
+             *       
+             * 4.public void DrawStairs(Canvas can, double width, int count)
+             * 
+             * 5.public void DrawRectangles(Canvas can, double width, int count)
+             * seitenlänge immer halbiert
+             * 
+        }
+    }
+}
